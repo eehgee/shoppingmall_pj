50f504b56dd6b133402a271cd6b27ac5f76fbc7d
Eunju Kim <wnqrx@naver.com>
2024-08-16 23:30:22 +0900

    url commit

--- src/store/productsData.ts
+++ src/store/productsData.ts
@@ -1,13 +1,13 @@
-import { useRecoilValueLoadable } from 'recoil';
-import { productsList } from '../store/products';
+// import { useRecoilValueLoadable } from 'recoil';
+// import { productsList } from '../store/products';
 
-const useProductsData = () => {
-  const productsLoadable = useRecoilValueLoadable(productsList);
+// const useProductsData = () => {
+//   const productsLoadable = useRecoilValueLoadable(productsList);
 
-  const loading = productsLoadable.state === 'loading';
-  const items = productsLoadable.state === 'hasValue' ? productsLoadable.contents : [];
+//   const loading = productsLoadable.state === 'loading';
+//   const items = productsLoadable.state === 'hasValue' ? productsLoadable.contents : [];
 
-  return { items, loading };
-};
+//   return { items, loading };
+// };
 
-export default useProductsData;
+// export default useProductsData;
